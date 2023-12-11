3911841ad828d1207ffc2b9cf05198ffd1c10833
Adrian <adrianbueno095@gmail.com>
2023-12-11 12:07:44 +0100

    Add new button Component

--- my-example/src/App.jsx
+++ my-example/src/App.jsx
@@ -1,10 +1,16 @@
 import './App.css';
+import Button from './components/Button';
 import Menu from './components/Menu';
 const App = () => {
   return (
     <>
       <Menu />
-      <h1>The Pocman</h1>
+      <h1>Say hello to ReactJs</h1>
+      <p>You will learn how to use the most popular frontend library, and become a super
+        Ninja developer.
+      </p>
+      <Button />
+
     </>
   )
 }
---
+++ my-example/src/components/Button.jsx
@@ -0,0 +1,6 @@
+const Button = () => {
+    return (
+        <button>Awesome</button>
+    )
+}
+export default Button;
